004e0678a6d5b8dba946657479071078f3b4fce4
zrj <kele3325@gmail.com>
2026-01-15 14:21:02 +0800

    feat:实现basechatmodel接口

--- model/chat_model.go
+++ model/chat_model.go
@@ -1,11 +1,25 @@
 package model
 
 import (
+	"context"
+
 	"github.com/cloudwego/eino-ext/components/model/ark"
 	"github.com/cloudwego/eino-ext/components/model/openai"
+	"github.com/cloudwego/eino/components/model"
+	"github.com/cloudwego/eino/schema"
 )
 
 type ChatModel struct {
 	Ark    ark.ChatModel
 	OpenAI openai.ChatModel
+}
+
+func (cm ChatModel) Generate(ctx context.Context, input []*schema.Message, opts ...model.Option) (*schema.Message, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (cm ChatModel) Stream(ctx context.Context, input []*schema.Message, opts ...model.Option) (*schema.StreamReader[*schema.Message], error) {
+	//TODO implement me
+	panic("implement me")
 }
